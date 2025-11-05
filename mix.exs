33f5dc8b49e5d3f8b10d7f5acea11d2651da7e03
Nicholas Henry <40253+nicholasjhenry@users.noreply.github.com>
2025-11-05 11:45:52 -0500

    chore: Add igniter

--- mix.exs
+++ mix.exs
@@ -44,6 +44,7 @@ defmodule FireStarter.Umbrella.MixProject do
       # Required to run "mix format" on ~H/.heex files from the umbrella root
       {:phoenix_live_view, ">= 0.0.0"},
       {:dialyxir, "~> 1.4", only: [:dev, :test], runtime: false},
+      {:igniter, "~> 0.5", only: [:dev, :test]},
       {:mix_test_watch, "~> 1.0", only: [:dev, :test], runtime: false}
     ]
   end
