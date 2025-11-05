c41376e08b9b4862f35943fae15dd34ea539acd0
Nicholas Henry <40253+nicholasjhenry@users.noreply.github.com>
2025-11-05 12:40:19 -0500

    feat: Add application macros

--- apps/fire_starter/lib/fire_starter.ex
+++ apps/fire_starter/lib/fire_starter.ex
@@ -8,4 +8,58 @@ defmodule FireStarter do
 
   <!--- LIST SUBDOMAINS HERE. -->
   """
+
+  defmodule Identifier do
+    @moduledoc """
+    Represents a identifier used to uniquely identify records.
+    """
+
+    @typedoc """
+    A unique identifier type, represented as an Ecto UUID.
+    """
+    @type t :: Ecto.UUID.t()
+  end
+
+  defmodule Attrs do
+    @moduledoc """
+    Represents attributes for record creation and updates.
+    """
+
+    @typedoc """
+    A map of attributes with either string or atom keys.
+    """
+    @type t() :: %{required(binary()) => term()} | %{required(atom()) => term()}
+  end
+
+  @doc false
+  def record do
+    quote do
+      use Ecto.Schema
+
+      alias FireStarter.Identifier
+
+      import Ecto.Changeset
+      import Ecto.Query, warn: false
+    end
+  end
+
+  @doc false
+  def context do
+    quote do
+      alias Ecto.Changeset
+
+      alias FireStarter.Attrs
+      alias FireStarter.Identifier
+      alias FireStarter.Repo
+
+      # alias FireStarter.Accounts.Scope
+
+      import Ecto.Query, warn: false
+    end
+  end
+
+  @doc false
+  defmacro __using__(which) when is_atom(which) do
+    apply(__MODULE__, which, [])
+  end
 end
